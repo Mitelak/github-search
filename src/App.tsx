ae7fd7b653d242489f95794b1594ff3c97ed519d
Mitelak <mitelak@gmail.com>
2021-02-23 09:29:56 +0100

    Handle search value in query string

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,26 @@ import Container from 'components/Container';
 
 import styles from './App.module.css';
 
+/*
+ * In an app with better routing handling, we could use query string as the only source of truth.
+ * In this case, it's only for keeping a better UX behavior.
+ */
+const SEARCH_QUERY_PARAM = 'search';
+const getInitialSearchName = (): string =>
+  new URLSearchParams(window.location.search).get(SEARCH_QUERY_PARAM) ?? '';
+
 const App = () => {
-  const [userName, setUserName] = React.useState<string>();
+  const [userName, setUserName] = React.useState<string>(getInitialSearchName);
+
+  const handleSearch = (name: string) => {
+    setUserName(name);
+    const query = new URLSearchParams({ [SEARCH_QUERY_PARAM]: name });
+    window.history.pushState({}, '', '?' + query.toString());
+  };
 
   return (
     <>
-      <Topbar onSearch={setUserName} />
+      <Topbar onSearch={handleSearch} />
       <Container className={styles.content}>
         {userName ? <UserDetails name={userName} /> : <Introduction />}
       </Container>
